61f48bd4dce631c4e215759d8adcf9773fb636f6
Nicolas Cabezas <nicolas.cabezas@udla.edu.ec>
2023-07-10 22:18:22 -0500

    traducciones

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -86,13 +86,13 @@
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Iniciar sesión') }}</a>
+                                    <a class="nav-link" href="{{ route('login') }}">Iniciar sesión</a>
                                 </li>
                             @endif
 
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Registrarse') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}">Registrarse</a>
                                 </li>
                             @endif
                         @else
